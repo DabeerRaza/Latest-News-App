63b40e6f76a218124073b650e73c71fc87fca2f0
Dabeer <dabeerofficial@gmail.com>
2019-09-30 16:45:22 +0500

    Updates

---
+++ precache-manifest.2a237fcdcf1c8c78e16dcc80f7217baa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac0df5a85a74a20981dc7824833e82b",
+    "url": "/latest-news-app/index.html"
+  },
+  {
+    "revision": "20822c1b1ae1a076a3ae",
+    "url": "/latest-news-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "01473fd4e3970475b4bb",
+    "url": "/latest-news-app/static/js/2.4d81d911.chunk.js"
+  },
+  {
+    "revision": "20822c1b1ae1a076a3ae",
+    "url": "/latest-news-app/static/js/main.82fcbe89.chunk.js"
+  },
+  {
+    "revision": "c3b9c58fa157a2c6ec3a",
+    "url": "/latest-news-app/static/js/runtime-main.23d91aba.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/latest-news-app/static/media/logo.25bf045c.svg"
+  }
+]);
